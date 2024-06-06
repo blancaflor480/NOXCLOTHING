18dabc5c245246fe924cf7137e465d5f2ae7dec4
Chanelle <137795318+Vio-lette@users.noreply.github.com>
2024-06-07 02:32:35 +0800

    upppp

--- user/contact.php
+++ user/contact.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap">
     <!-- Custom StyleSheet -->
     <link rel="stylesheet" href="./css/styles.css" />
     <title>Contact</title>
@@ -32,7 +33,7 @@
       }
 
       .contact-section {
-        width: 48%;
+        width: 49%;
       }
 
       h1 {
@@ -78,10 +79,26 @@
         margin-top: 20px;
         font-size: 16px;
       }
+      .contact-details{
+  background-color: #4c4c4c;
 
-      .contact-details h2 {
-        margin: 10px 0;
+ }
+      .contact-details h3 {
+      
+        color:  white;
+        
       }
+
+      .contact-details h1 {
+        margin: 10px 0;
+        color:  white;
+        
+      }
+      hr {
+    border: none;
+    height: 2px;
+    background-color: white;
+}
     </style>
   </head>
 
@@ -147,7 +164,7 @@
     </div>
 
     <center>
-      <b><h1>Connect with us</h1></b>
+      <h1>Connect with us</h1>
       <div class="contact-container">
         <div class="contact-section">
           <form id="contactForm">
@@ -166,12 +183,13 @@
         </div>
 
         <div class="contact-section contact-details">
-          <b>
-            <h1>Contact Details:</h1>
-            <h2>Email: aubrianachanelle.buyo@cvsu.edu.ph</h2>
-            <h2>Telephone Number: 456-777-451</h2>
-            <h2>Mobile Number: 0912345678</h2>
-          </b>
+       
+            <h1>Let's Connect!</h1>
+            <hr>
+            <h3>Email: aubrianachanelle.buyo@cvsu.edu.ph</h3>
+            <h3>Telephone Number: 456-777-451</h3>
+            <h3>Mobile Number: 0912345678</h3>
+      
         </div>
       </div>
     </center>
